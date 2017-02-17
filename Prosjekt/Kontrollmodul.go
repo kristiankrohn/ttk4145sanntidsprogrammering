a68ae07b7cd1ce79331ae8d0fb9eecb7b0524da2
siverst <siverst@stud.ntnu.no>
2017-02-17 14:28:53 +0100

    added comments

--- Prosjekt/Kontrollmodul.go
+++ Prosjekt/Kontrollmodul.go
@@ -1,28 +1,36 @@
 package main
 
 import (
-  
+
 
 )
 
 func Beregn_kostnad(){
-
+  //gjør beregninger - eksterne og interne ordre
+  //del resultat på nettverk
 }
 
 func Ekstern_ordre(){
-
+  //check om ordre allerede er i køen
+  //legg til hvis ikke
+  //del ordre på nettverket
 }
 
 func Inkommende_ordre(){
-
+  //check om ordre allerede er i køen
+  //legg til hvis ikke
+  //fjern intern ordre fra kø når ordren er fullført
 }
 
 func Vurder_kostnad(){
-
+  //sammenlign innkommende resultat
+  //ta ordre eller la være hvis noen andre har bedre kost
 }
 
 func Kvitter_ordre(){
-
+  //send kvittering for ekstern ordre på nettverket
+  //motta kvittering
+  //fjern ordre fra kø
 }
 
 func main(){

8acb7e7be21f52f19f58956e38211049dcd92567
Kristian Krohn <kristianhk@msn.com>
2017-02-17 14:23:35 +0100

    added doorlamp

--- Prosjekt/Heismodul.go
+++ Prosjekt/Heismodul.go
@@ -183,7 +183,7 @@ func GoToFloor(currentFloor int, nextFloor int) bool {
 	if floor == nextFloor {
 		Elev_set_motor_direction(DIRN_STOP)
 		Elev_set_door_open_lamp(1)
-		time.Sleep(Second*1)
+		time.Sleep(time.Second*1)
 		Elev_set_door_open_lamp(0)
 		return true
 	}
